db4fbaba6fe6b666829cf5cb04c2ba0f832e05b2
amana <aman@appitronsolutions.com>
2024-09-25 16:31:02 +0530

    asfdss

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2714901465"
+    serviceWorkerVersion: "3746430660"
   }
 });
